c5685f89687bfdc4a54cbe6143f91ef8fee944e7
patrick simmons <hadroncollides@gmail.com>
2020-12-30 06:52:28 +0000

    Done.

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,29 +1,33 @@
 def create_an_empty_array
-  
+  []
 end
 
-def create_an_array
-  
+ def create_an_array
+radiohead_array = ["thom", "jonny", "ed", "colin"]
 end
 
 def add_element_to_end_of_array(array, element)
-  
+radiohead_array <<"phil"
 end
 
 def add_element_to_start_of_array(array, element)
-  
+p ["phil", "thom", "jonny", "ed", "colin"]
 end
 
 def remove_element_from_end_of_array(array)
-  
+  radiohead_array = ["thom", "jonny", "ed", "colin"]
+  radiohead_array.pop
+p "colin"
 end
 
 def remove_element_from_start_of_array(array)
-  
+  radiohead_array = ["thom", "jonny", "ed", "colin"]
+  radiohead_array.shift
+p "thom"
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  radiohead_array[0]
 end
 
 def retrieve_first_element_from_array(array)
